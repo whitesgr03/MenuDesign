8811e742ebd06184ae5667670dda67b61cb0fee6
Weiss Bai <whitesgr03@gmail.com>
2022-11-28 15:58:06 +0800

    Add create hamburger menu

--- src/components/main.js
+++ src/components/main.js
@@ -4,9 +4,15 @@ import '../css/main.css';
 
 
 import { createDropdown } from "./dropdown";
+import { createHamburger } from "./hamburger";
 
 const main = (() => {
+
     // catch DOM
+    const hamburgerIcon = document.querySelector('.hamburger .icon')
+    
+    createHamburger.activeHamburger(hamburgerIcon);
+        
     document.addEventListener("pointerover", activeMenu);
 
     function activeMenu(e) {
