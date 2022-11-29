6f4e5904bd4e846e02a53c505b64b0a55cae5521
Weiss Bai <whitesgr03@gmail.com>
2022-11-29 15:51:59 +0800

    Add tabs script

---
+++ src/components/tabs.js
@@ -0,0 +1,24 @@
+"use strict";
+
+import "../css/tabs.css";
+
+const createTabs = (() => {
+    const activeTab = (menu, tab) => {
+
+        if (!tab) return;
+
+        const currentActiveTab = menu.querySelector('.active');
+
+        if (currentActiveTab) {
+            currentActiveTab.classList.remove("active");
+        }
+
+        tab.classList.add("active");
+    };
+
+    return {
+        activeTab,
+    };
+})();
+
+export { createTabs };
