04b3f525fcc876cc8aed06716e71901fbf939491
Weiss Bai <whitesgr03@gmail.com>
2022-11-29 13:50:47 +0800

    Update main.js

--- src/components/main.js
+++ src/components/main.js
@@ -1,24 +1,27 @@
 "use strict";
 
-import '../css/main.css';
-
+import "../css/main.css";
 
 import { createDropdown } from "./dropdown";
 import { createHamburger } from "./hamburger";
 
 const main = (() => {
+    document.addEventListener("pointerdown", activeMenuOnClick);
+    document.addEventListener("pointerover", activeMenuOnOver);
 
-    // catch DOM
-    const hamburgerIcon = document.querySelector('.hamburger .icon')
-    
-    createHamburger.activeHamburger(hamburgerIcon);
-        
-    document.addEventListener("pointerover", activeMenu);
-
-    function activeMenu(e) {
+    function activeMenuOnOver(e) {
         if (e.target.closest(".dropdown")) {
+
             const dropdown = e.target.closest(".dropdown");
             createDropdown.activeItemHover(dropdown);
+        }
+    }
+
+    function activeMenuOnClick(e) {
+        if (e.target.closest(".hamburger")) {
+
+            const hamburger = e.target.closest(".hamburger");
+            createHamburger.activeHamburger(hamburger);
         }
     }
 })();
