aac07d9c14983081a2bc27f97939df8f8fb3d2e8
Weiss Bai <whitesgr03@gmail.com>
2022-12-02 16:54:02 +0800

    Refactor hamburger to be a factory function and make the code cleaner

--- src/components/hamburger.js
+++ src/components/hamburger.js
@@ -2,39 +2,26 @@
 
 import "../css/hamburger.css";
 
-const createHamburger = (() => {
-    const activeHamburger = (hamburger) => {
-        if (!hamburger.classList.contains("active")) {
-            hamburger.classList.add("active");
-            setTimeout(() => {
-                document.addEventListener("click", closeMenu);
-            });
-        }
+const createHamburger = (hamburger) => {
+    const list = hamburger.querySelector(".list");
+
+    const activeHamburger = () => {
+        if (hamburger.classList.contains("active")) return;
+
+        hamburger.classList.add("active");
 
         function closeMenu(e) {
-            if (
-                e.target.closest(".list") &&
-                e.target.closest(".list") === hamburger.querySelector(".list")
-            )
-                return;
+            if (e.target.closest(".list") === list) return;
 
-            const icon = hamburger.querySelector(".icon");
-
-            if (
-                (hamburger.classList.contains("active") &&
-                    e.target.closest(".icon") === icon) ||
-                e.target.closest(".icon") !== icon
-            ) {
-                hamburger.classList.remove("active");
-            }
-
-            document.removeEventListener("click", closeMenu);
+            hamburger.classList.remove("active");
+            console.log('remove')
+            document.removeEventListener("pointerup", closeMenu);
         }
     };
 
     return {
         activeHamburger,
     };
-})();
+};
 
 export { createHamburger };
