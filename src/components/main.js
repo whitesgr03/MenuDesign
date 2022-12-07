41bf71a56fc02c8219cd6fa3382bf183a038ab06
Weiss Bai <whitesgr03@gmail.com>
2022-12-07 18:17:32 +0800

    Update main
    The document listens to the pointDown event to trigger the cyclic
    carousel scrolling

--- src/components/main.js
+++ src/components/main.js
@@ -6,11 +6,38 @@ import { createDropdown } from "./dropdown";
 import { createHamburger } from "./hamburger";
 import { createTabs } from "./tabs";
 import { createCarousel } from "./carousel";
+import { createCycleCarousel } from "./circularCarousel";
+import { handleCarouselList } from "./handleCarouselList";
+
+import image from "../img/big1.jpg";
+import image2 from "../img/big2.jpg";
+import image3 from "../img/big3.jpg";
+import image4 from "../img/big4.jpg";
+import image5 from "../img/big5.jpg";
+import image6 from "../img/big6.jpg";
 
 const main = (() => {
+    const circularCarousel = document.querySelector(".carousel.circular");
+
+    const sources = getSources();
+    const halfLength = Math.floor(sources.length / 2);
+    const itemWidth = 500;
+
+    const carouselList = handleCarouselList(
+        circularCarousel,
+        sources,
+        itemWidth
+    );
+    carouselList.createItem();
+    carouselList.createDot();    
+
     document.addEventListener("pointerover", activeMenuOnPointerOver);
     document.addEventListener("pointerdown", activeMenuOnPointerDown);
     document.addEventListener("pointerup", activeMenuOnPointerUp);
+
+    function getSources() {
+        return [image, image2, image3, image4, image5, image6];
+    }
 
     function activeMenuOnPointerOver(e) {
         if (e.target.closest(".dropdown")) {
@@ -34,17 +61,52 @@ const main = (() => {
             }
         }
     }
+    
     function activeMenuOnPointerDown(e) {
         if (e.target.closest(".carousel")) {
-            const carousel = createCarousel(e.target.closest(".carousel"));
+            let carousel = e.target.closest(".carousel");
 
-            if (e.target.closest(".arrow")) {
-                const arrow = e.target.closest(".arrow").classList[1];
-                carousel.scrollByClick(arrow);
-            }
+            if (carousel.classList.contains("circular")) {
 
-            if (e.target.closest(".list")) {
-                carousel.scrollByPointerMove(e);
+                const newCarousel = createCycleCarousel(
+                    carousel,
+                    halfLength,
+                    itemWidth
+                );
+                
+                if (e.target.closest(".arrow")) {
+                    const direction = e.target.closest(".arrow").classList[1];
+                    newCarousel.scrollByClick(direction);
+                }
+
+                if (e.target.closest(".dot")) {
+                    const currentDotIndex =
+                        +carousel.querySelector(".dot.active").dataset.dotIndex;
+
+                    const selectDotIndex =
+                        +e.target.closest(".dot").dataset.dotIndex;
+
+                    const index = selectDotIndex - currentDotIndex;
+
+                    if (index === 0) return;
+
+                    let direction = null;
+
+                    if (index > 0) {
+                        direction = "next";
+                    } else {
+                        direction = "prev";
+                    }
+
+                    for (let i = 1; i <= Math.abs(index); i++) {
+                        newCarousel.scrollByClick(direction, true);
+                    }
+                }
+            } else {
+                const newCarousel = createCarousel(carousel, 60, 4);
+                if (e.target.closest(".list")) {
+                    newCarousel.scrollByPointerMove(e);
+                }
             }
         }
     }
